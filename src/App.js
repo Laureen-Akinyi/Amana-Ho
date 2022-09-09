11e21302e6b6eaa9c408e8d04e62a0b1ad0a6f4d
laureen <laureen.akinyi@student.moringaschool.com>
2022-09-09 14:51:01 +0300

    initialized doctors component in Doctors.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Container, Row, Col } from 'reactstrap'
 import ModalForm from './Components/Modals/Modal'
 import DataTable from './Components/Tables/DataTable'
+// import { BrowserRouter, Route } from "react-router-dom";
 
 
 class App extends Component {
@@ -66,5 +67,19 @@ class App extends Component {
     )
   }
 }
+
+// function App() {
+//   return (
+
+//   <BrowserRouter>
+//   <Route>
+//     <Route exact path="/">
+//       <Home />
+//   </Route>
+//   </BrowserRouter>
+
+//  ); 
+
+// }
 
 export default App
